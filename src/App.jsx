63129093ab6641cbd268a2a4e1f5bb37392746e9
ManupaDev <manupadevsuite@gmail.com>
2024-06-30 22:58:52 +0530

    feat: week-4 compelte

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Hero from "./components/Hero"; //! ./ => Inside the same folder
 import Navigation from "./components/Navigation";
+import Products from "./components/Products";
 
 function App() {
-
   const name = "Manupa";
 
   return (
     <main>
       <Navigation name={name} />
       <Hero />
+      <Products />
     </main>
   );
 }
